3f9e50ce91b3d2a743f792f0d697c00f83fae8f3
Cong <congjeezy@gmail.com>
2025-04-15 10:25:22 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+
+#import blueprints
+from controllers.read_student_controller import read_bp
+from controllers.create_student_controller import create_bp
+
+#initialize the Flask application
+app = Flask(__name__)
+
+
+#register blueprints
+app.register_blueprint(read_bp)
+app.register_blueprint(create_bp)
+
+
+
+
+app.secret_key="your_secret_key"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
