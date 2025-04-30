77167cfc324629cbd29579956b4751269eb2c7a0
Cong <congjeezy@gmail.com>
2025-04-30 16:20:53 -0500

    -Image Upload
    - added upload feature for images for create, update and delete.

--- controllers/create_student_controller.py
+++ controllers/create_student_controller.py
@@ -4,6 +4,11 @@ import re
 
 create_bp = Blueprint("create_student", __name__)
 
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 def validate_input(data):
     errors = []
     if not  data.get("name") or not re.match(r'^[A-Za-z ]+$', data["name"]):
@@ -24,13 +29,20 @@ def add_student_route():
 
     errors = validate_input(student_data)
 
+    image_file = request.files.get("image")
+    if image_file and image_file.filename != "":
+        if not allowed_file(image_file.filename):
+            errors.append("Invalid image format. only png, jpg, jpeg, gif are allowed.")
+    else:
+        image_file = None
+
     #rerender if errors
     if errors:
         students = get_all_students()
-        return render_template('students.html', students=students, errors=errors)
+        return render_template('students.html', students=students, errors=errors, student=student_data)
 
     #if no errors add to db
-    add_student(student_data)
+    add_student(student_data, image_file)
     flash('Student added successfully!', 'success')
 
     return redirect(url_for("read_student.index"))
